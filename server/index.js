9ebf8967fa3df857b9e4d2b7ad9ec39c9c39d0e7
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-02-01 19:59:57 +0530

    updated client dependencies

--- server/index.js
+++ server/index.js
@@ -20,10 +20,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
@@ -136,5 +139,9 @@ app.get("/post/:id", async (req, res) => {
   res.json(postDoc);
 });
 
-app.listen(4000);
-//
+// Start the server
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,18 @@
-const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
-  username: {type: String, required: true, min: 4, unique: true},
-  password: {type: String, required: true},
+  username: { type: String, required: true, min: 4, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-const UserModel = model('User', UserSchema);
+UserSchema.pre("save", function (next) {
+  this.username = this.username.toLowerCase();
+  this.email = this.email.toLowerCase();
+  next();
+});
+
+const UserModel = model("User", UserSchema);
 
 module.exports = UserModel;
