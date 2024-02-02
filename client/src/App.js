014813e556f6bcbaea640d25012b2184675ff3bf
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-02-03 01:18:11 +0530

    Added media and public files

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import Post from "./Post";
-import Header from "./Header";
-import {Route, Routes} from "react-router-dom";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
+import BlogsPage from "./pages/BlogsPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
-import {UserContextProvider} from "./UserContext";
+import { UserContextProvider } from "./UserContext";
 import CreatePost from "./pages/CreatePost";
 import PostPage from "./pages/PostPage";
 import EditPost from "./pages/EditPost";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -17,12 +17,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />} />
+          <Route path="/blogs" element={<BlogsPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/create" element={<CreatePost />} />
           <Route path="/post/:id" element={<PostPage />} />
           <Route path="/edit/:id" element={<EditPost />} />
         </Route>
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </UserContextProvider>
   );
